18f36481f8c0203f772f97512c96cc9fa0347f0e
linzs <linjensor@gmail.com>
2017-07-11 09:09:07 +0800

    blog core and backend project

---
+++ blog-backend/src/main/java/cn/linzs/BlogBackendApplication.java
@@ -0,0 +1,12 @@
+package cn.linzs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackendApplication.class, args);
+	}
+}
---
+++ blog-core/src/main/java/cn/linzs/BlogCoreApplication.java
@@ -0,0 +1,12 @@
+package cn.linzs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCoreApplication.class, args);
+	}
+}
